e40ec609d224510742b142eeb10ba932511ea5e2
CLAB01-PCC28 <you@example.com>
2024-10-04 08:47:34 +0800

    activity

---
+++ activity/BasicOutput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Our Lady of Lourdes College Foundation" << endl;
+    cout << "College Computer Studies" << endl;
+    cout << "Prince Edward L. Romano" << endl;
+    cout << "Brgy. Laniton Basud Camarines Norter";
+    return 0;
+}
