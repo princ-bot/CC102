f585b33723d524179eb1907df7345ef85de74791
CLAB01-PCC28 <you@example.com>
2024-10-07 09:30:08 +0800

    activities

---
+++ CC102/BasicInput.cpp
@@ -0,0 +1,13 @@
+#inlude <iostream>
+using namespace std
+
+int main() {
+    string nickname, course_yearlevel, school;
+    cout << "Nickname: ";
+    cin >> nickname;
+    cout << "Course-year level: ";
+    cin course_yearlevel;
+    cout << "School: ";
+    cin >> school;
+    return 0;
+}
---
+++ CC102/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "HOWDY LOURDENIAN PROGRAMMERS!";
+    return 0;
+}
