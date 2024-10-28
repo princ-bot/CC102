4f48b11badee44f3ea4e07880433b1b2901d656b
princ <162914951+princ-bot@users.noreply.github.com>
2024-10-28 19:52:18 +0800

    my activity

---
+++ Personal Activity/character_ascii_converter.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+void decimalToBinary(int number) {
+    int binaryNumber[64];
+    int i = 0;
+    while (number > 0) {
+        binaryNumber[i] = number % 2;
+        number = number / 2;
+        i++;
+    }
+    for (int j = i - 1; j >= 0; j--) {
+        cout << binaryNumber[j];
+    }
+}
+
+void decimalToOctal(int number) {
+    int octalNumber[64];
+    int i = 0;
+    while (number > 0) {
+        octalNumber[i] = number % 8;
+        number = number / 8;
+        i++;
+    }
+    for (int j = i - 1; j >= 0; j--) {
+        cout << octalNumber[j];
+    }
+}
+
+int main() {
+    char character;
+    cout << "Enter the Character: ";
+    cin >> character;
+    int decimal = int(character);
+    cout << "Decimal: " << decimal << endl;
+    cout << "Binary: ";
+    decimalToBinary(decimal);
+    cout << endl;
+    cout << "Hexadecimal: " << hex << decimal << endl;
+	 cout << "Octal: ";
+	 decimalToOctal(decimal);
+    return 0;
+}
