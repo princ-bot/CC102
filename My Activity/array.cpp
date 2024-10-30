9d2c979e62ff68b943c0b45d8c5d70d1ee87607a
princ <162914951+princ-bot@users.noreply.github.com>
2024-10-30 19:21:13 +0800

    Some array

--- My Activity/array.cpp
+++ My Activity/array.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 	int number[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 	int n = sizeof(number) / sizeof(number[0]);
 	int sum;
@@ -12,15 +13,18 @@ int main() {
 	cout << "Enter sum: ";
 	cin >> sum;
 	bool pairFound = false;
-	for (int i = 0; i < n - 1 && !pairFound; i++) {
-    for (int j = i + 1; j < n; j++) {
-        if (number[i] + number[j] == sum) {
-            cout << "Pair: (" << number[i] << ", " << number[j] << ")\n";
+	for (int i = 0; i < n - 1 && !pairFound; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (number[i] + number[j] == sum)
+			{
+				cout << "Pair: (" << number[i] << ", " << number[j] << ")\n";
 				pairFound = true;
 				break;
-        }
-    }
-}
+			}
+		}
+	}
 
 	return 0;
 }
