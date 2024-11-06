3de2f22485dca530243b90da422018c0c1177c04
princ <162914951+princ-bot@users.noreply.github.com>
2024-11-06 19:17:37 +0800

    Binary to Decimal

--- My Activity/binary_to_decimal.cpp
+++ My Activity/binary_to_decimal.cpp
@@ -34,3 +34,15 @@ int main() {
 	}
 	return 0;
 }
+
+/* ------- How it Work ---------- */
+/*
+	 Binary is base 2
+	  0   1   0   0   0   0   0   1
+	 2^8 2^7 2^6 2^5 2^4 2^3 2^2 2^1
+	 128 64  32   16  8   4   2   1
+	     64                       1
+
+	 binary_Result = 64 + 1 = 65 in decimal which is capital A
+	 using (char)decimal, to convert decimal to character
+ */
