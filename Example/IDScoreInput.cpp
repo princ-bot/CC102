967f46e4155635457c0df47f9a08f6efb8a37464
princ <162914951+princ-bot@users.noreply.github.com>
2024-11-06 18:37:10 +0800

    ID and Score Input

---
+++ Example/IDScoreInput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int id, score; // declare id and score
+	cout << "Enter student ID and score: ";
+	cin >> id >> score; // Get user input
+	cout << "Student ID: " << id << " Score " << score << endl; // display id and score
+	return 0;
+}
