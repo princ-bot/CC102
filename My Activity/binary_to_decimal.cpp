8cd8c717b5f0dde5e6c54d6ce47c4715e189a5f6
princ <162914951+princ-bot@users.noreply.github.com>
2024-11-08 17:08:21 +0800

    404 Not Found

--- My Activity/binary_to_decimal.cpp
+++ My Activity/binary_to_decimal.cpp
@@ -1,28 +1,8 @@
 #include <iostream>
-#include <string>
-#include <cmath>
-#include <algorithm>
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
-	cout << "Error: 404 Not Found";
-=======
- 	Error 404 not found
->>>>>>> 5bc432e95f5398b1536af0bcc16c6ff5f459e2b9
+	cout << "Error 404 Not Found" << endl;
+	cout << "Soon.....";
+	return 0;
 }
-
-/* ------- How it Work ---------- */
-/*
-	 Binary is base 2
-	  0   1   0   0   0   0   0   1
-	 2^7 2^6 2^5 2^4 2^3 2^2 2^1 2^0
-	 128 64  32   16  8   4   2   1
-	     64                       1
-<<<<<<< HEAD
- */
-=======
-
-	 
- */
->>>>>>> 5bc432e95f5398b1536af0bcc16c6ff5f459e2b9
