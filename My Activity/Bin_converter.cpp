ebca3be37b18005ec6cd829587d622781f54a1c9
princ <162914951+princ-bot@users.noreply.github.com>
2024-11-21 22:02:07 +0800

    Hakdog

---
+++ My Activity/Bin_converter.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main() {
+	string in;
+    string r = "";
+    int d = 0;
+	 cout << "Enter Bin: ";
+	 getline(cin, in);
+
+    for (char c : in) {
+        if (c != ' ') {
+            r += c;
+        } else {
+            for (int j = 0; j < r.length(); ++j) {
+                r[r.length() - j - 1] == '1' ? d += pow(2, j) : 0;
+            }
+            cout << (char)d;
+            r.clear();
+            d = 0;
+        }
+    }
+
+    if (!r.empty()) {
+        for (int j = 0; j < r.length(); ++j) {
+            r[r.length() - j - 1] == '1' ? d += pow(2, j) : 0;
+        }
+        cout << (char)d;
+    }
+
+    return 0;
+}
