c9171f19b7502f033b66b01bfdd541f4c521bcb3
Prince Edward Romano <162914951+princ-bot@users.noreply.github.com>
2024-11-22 17:06:14 +0800

    age.cpp

--- School Activity/age.cpp
+++ School Activity/age.cpp
@@ -1,41 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-BEGIN
-    DECLARE age AS INTEGER
-    OUTPUT "What is your age: "
-    INPUT age
-
-    IF age < 16 THEN
-        OUTPUT "Too young to drive."
-    ELSE IF age == 16 THEN
-        OUTPUT "Better clear the road."
-    ELSE
-        OUTPUT "You are getting old."
-    END IF
-END
------------------------------------
-
-Start: Begin the process.
-
-Input: Get user input for age.
-
-Decision: Check if age < 16.
-
-Yes: Output "Too young to drive."
-
-No: Go to the next decision.
-
-Decision: Check if age == 16.
-
-Yes: Output "Better clear the road."
-
-No: Output "You are getting old."
-
-End: End the process.
-*/
-
 int main() {
 	int age;
 	cout << "What is you age: ";
