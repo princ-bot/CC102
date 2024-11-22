6855ba7343391632e971c6f6eeb5a614d2bbdc4b
princ <162914951+princ-bot@users.noreply.github.com>
2024-11-22 17:21:18 +0800

    Activity 1

--- School Activity/temperature.cpp
+++ School Activity/temperature.cpp
@@ -3,9 +3,6 @@ using namespace std;
 
 int main() {
 	int temp;
-	cout << "Enter temp: ";
-	cin >> temp;
-	
-	cout << (temp <= 32 ? "Bring a heavy jacket." : temp >= 32 && temp <= 50 ? "Bring a light jacket." : temp >= 50 ? "Bring any jacket." : 0);
+	cout << (cout << "Enter temp: ", cin >> temp, temp <= 32 ? "Bring a heavy jacket." : temp >= 32 && temp <= 50 ? "Bring a light jacket." : temp >= 50 ? "Bring any jacket." : 0);
 	return 0;
 }
