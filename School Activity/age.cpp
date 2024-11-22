4f68e106e652a58d93df2f60739442bc1f0dfb1d
CLAB01-PCC28 <you@example.com>
2024-11-22 09:34:54 +0800

    Deleted activity 3

---
+++ School Activity/age.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int age;
+	cout << "What is you age: ";
+	cin >> age;
+	cout << (age < 16 ? "Too young to drive." : age == 16 ? "Better clear the road." : age > 16 ? "You are getting old." : 0);
+	return 0;
+}
