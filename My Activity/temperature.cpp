87407666cad4bf7c349591d1cacbedfb51a7d39f
CLAB01-PCC28 <you@example.com>
2024-11-22 09:14:47 +0800

    Activity 1

---
+++ My Activity/temperature.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int temp;
+	cout << "Enter temp: ";
+	cin >> temp;
+	cout << (temp <= 32 ? "Bring a heavy jacket." : temp >= 32 && temp <= 50 ? "Bring a light jacket." : temp >= 50 ? "Bring any jacket." : 0);
+	return 0;
+}
