96c197c482ebf4addce7360eafba93d7da5d7db7
princ <162914951+princ-bot@users.noreply.github.com>
2024-11-22 17:24:23 +0800

    Activity 3

--- School Activity/movie.cpp
+++ School Activity/movie.cpp
@@ -5,8 +5,6 @@ int main() {
 	int age;
 	float money;
 	string with_parent;
-	cout << (cout << "Enter age: ",
-	cin >> age, age <= 13 ? (cout << "With parent? ", cin >> with_parent, with_parent == "yes" ? "G, PG\n" : "G\n") : age >= 13 && age <= 16 ? (cout << "With parent? ", cin >> with_parent,with_parent == "yes" ? "G, PG, R\n" : "G, PG\n") : age >= 16 ? "G, PG, R\n" : 0);
-	cout << (cout << "Enter amoung of money: ", cin >> money, money <= 7.50 ? "Matinee." : money >= 7.50 ? "Evening" : 0);
+	cout << (cout << "Enter age: ", cin >> age, age <= 13 ? (cout << "With parent? ", cin >> with_parent, with_parent == "yes" ? "G, PG\n" : "G\n") : age >= 13 && age <= 16 ? (cout << "With parent? ", cin >> with_parent,with_parent == "yes" ? "G, PG, R\n" : "G, PG\n") : age >= 16 ? "G, PG, R\n" : 0) << (cout << "Enter amoung of money: ", cin >> money, money <= 7.50 ?"Matinee." : money >= 7.50 ? "Evening" : 0);
 	return 0;
 }
