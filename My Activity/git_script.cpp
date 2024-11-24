25ef4b70599a5af7468ab4d6d8e84bef9996350b
princ <162914951+princ-bot@users.noreply.github.com>
2024-11-24 17:53:19 +0800

    My Github Script for Uploading file cause im lazy

---
+++ My Activity/git_script.cpp
@@ -0,0 +1,64 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+void run_command(const string& command) {
+    int result = system(command.c_str());
+    if (result != 0) {
+        cerr << "Error..." << command << endl;
+        exit(result);
+    }
+}
+
+int main() {
+    int user_choices;
+    string user_branch_name;
+
+    while (true) {
+        int user_choices;
+
+        cout << "====== Welcome to Git Command Automation ===== " << endl;
+        cout << "1. Pull Request\n" << "2. Upload\n" << "3. Delete\n" << "4. Exit\n";
+        cin >> user_choices;
+        cin.ignore();
+
+        if (user_choices == 1) {
+            try {
+                run_command("git pull pr main");
+            } catch (const exception& e) {
+                cerr << "Eror... " << e.what() << endl;
+            }
+        } else if (user_choices == 2) {
+            string file_name;
+            string commit_message;
+            cout << "Enter Filename: ";
+            getline(cin, file_name);
+            cout << ("Enter Commit: ");
+            getline(cin, commit_message);
+            try {
+                run_command("git add " + file_name);
+                run_command("git commit -m \"" + commit_message + "\"");
+                run_command("git push pr main"); // Replace with your remote name and branch name ("git push [remote name] [branch name]")
+                cout << "Upload Completed..." << endl;
+            } catch (const exception& e) {
+                cerr << "Error..." << e.what() << endl;
+            }
+        } else {
+            string file_name;
+            string commit_message;
+            cout << "Enter Filename to delete: ";
+            getline(cin, file_name);
+            cout << "Enter Commit Message: ";
+            getline(cin, commit_message);
+            try {
+                run_command("git rm -r " + file_name);
+                run_command("git commit -m \"" + commit_message + "\"");
+                run_command("git push -u pr main");
+                cout << "File Deleted Successfully...";
+            } catch (const exception& e) {
+                cerr << "Error.. " << e.what() << endl;
+            }
+        }
+    }
+    return 0;
+}
