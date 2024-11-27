4f5cde1bfb5e17840040d0b0bec1886a52b158f4
princ <162914951+princ-bot@users.noreply.github.com>
2024-11-27 18:16:49 +0800

    PreFinal & Final

---
+++ School Activity/activity1/c.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "  ";
+        }
+        for (int j = i + 1; j < 8; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
