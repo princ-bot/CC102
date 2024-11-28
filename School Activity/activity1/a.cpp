99eef524f26cfb7b1aa461728247ec0e8b803cbc
princ <162914951+princ-bot@users.noreply.github.com>
2024-11-29 05:39:32 +0800

    PreFinal & Final

--- School Activity/activity1/a.cpp
+++ School Activity/activity1/a.cpp
@@ -2,13 +2,9 @@
 using namespace std;
 
 int main() {
-	for (int i = 01; i < 9; ++i) {
+	for (int i = 0; i < 9; ++i) {
 		for (int j = 1; j < 8; ++j) {
-			if ((i + j) % 2 == 0) {
-				cout << "# *";
-			} else {
-				cout << " # * ";
-			}
+			cout << ((i + j) % 2 == 0 ? "# *" : " # * ");
 		}
 		cout << endl;
 	}
