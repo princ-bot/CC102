04de7637d5c27b8dd3edf814933c4180d74728eb
princ <162914951+princ-bot@users.noreply.github.com>
2024-11-29 05:02:27 +0800

    PreFinal & Final

--- School Activity/activity1/a.cpp
+++ School Activity/activity1/a.cpp
@@ -5,9 +5,9 @@ int main() {
 	for (int i = 01; i < 9; ++i) {
 		for (int j = 1; j < 8; ++j) {
 			if ((i + j) % 2 == 0) {
-				cout << "#";
+				cout << "# *";
 			} else {
-				cout << " * ";
+				cout << " # * ";
 			}
 		}
 		cout << endl;
